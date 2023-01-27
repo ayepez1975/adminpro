4ddfd01cb2812c0b36e0ea4abe3118b6b4ed5945
ayepez <ayepez@swplus.com.mx>
2023-01-27 13:52:48 -0600

    Angular AdminPro Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
 
 import { AppComponent } from './app.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotpagefoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+        PagesModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
